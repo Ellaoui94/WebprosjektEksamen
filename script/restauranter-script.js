05a95ade269c8f534f30287edd1e74d0ea20bc52
Generoman <roman181@gmail.com>
2021-05-22 16:33:27 +0200

    Krangler med CSS

--- script/restauranter-script.js
+++ script/restauranter-script.js
@@ -1,7 +1,7 @@
 import RestaurantModule from "./modules/RestaurantModule.js";
 // import AnsatteModule from "./modules/AnsatteModule.js";
 
-let testSection = document.querySelector("#test-section");
+let restaurantSection = document.querySelector("#restaurant-section");
 
 // let alleRestauranter = RestaurantModule.getAllRestauranter();
 
@@ -14,16 +14,16 @@ const generateRestauranter = () => {
         // Denne delen er generelt uferdig
         htmlText += `
             <article id="${restaurant.id}" class="column">
-                <div class="card">
+                <div class="card restaurant-card">
                     <section class="card-header">
-                        <i class="card-header-icon">ic</i>
-                        <h3 class="card-header-title is-centered">${restaurant.navn}</h3>
+                        
+                        <h3 class="card-header-title is-centered"><span class="card-header-icon">(ic)</span>${restaurant.navn}</h3>
                     </section>
                     <section class="card-image">
-                        <img src="images/${restaurant.bilde}" alt="Placeholder image">
+                        <img src="images/${restaurant.bilde}" alt="bilde av restaurant">
                     </section>
                     <section class="card-content">
-                        <ul>
+                        <ul class="content">
                             <li>ID: ${restaurant.id}</li>
                             <li>Adresse: ${restaurant.adresse}</li>
                             <li>Telefon: ${restaurant.telefon}</li>
@@ -31,17 +31,16 @@ const generateRestauranter = () => {
                             <li>Leder: ${restaurant.leder[0]}</li>
                         </ul>
                     </section>
-                    <section class="card-content">
-    
-                        SETT REDIGER / SLETT KNAPP HER
-    
+                    <section class="card-footer">
+                        <button type="button" class="card-footer-item edit-button"><span>(ic)</span>Rediger</button>
+                        <button type="button" class="card-footer-item delete-button"><span>(ic)</span>Slett</button>
                     </section>
                 </div>
             </article>
         `;
     });
 
-    testSection.innerHTML = htmlText;
+    restaurantSection.innerHTML = htmlText;
 
 };
 
