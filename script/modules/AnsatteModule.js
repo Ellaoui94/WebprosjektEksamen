547b0b93d427737ec632f5fc87bab86b888fa640
Marina1014 <makh004@student.kristiania.no>
2021-05-22 17:10:45 +0200

    Laget ny funksjonalitet og endret

--- script/modules/AnsatteModule.js
+++ script/modules/AnsatteModule.js
@@ -3,41 +3,52 @@ const AnsatteModule = (function(){
     const ansatte = [
         {
             // Denne skal ikke brukes i det ferdige produktet
-            id: 00000,
+            id: "00000",
             navn: "PLACE HOLDER",
             alder: 30,
             stilling: "Ansatt",
             arbeidssted: "RESTAURANTNAVN",
             stillingsprosent: 100,
             timelønn: 200,
-            bilde: "bildefil.png"
+            bilde: "bildefil.png",
+            telefon: "92356087"
         },
         {
             // Denne skal ikke brukes i det ferdige produktet
-            id: 00001,
+            id: "00001",
             navn: "SAMPLE NAME",
             alder: 30,
             stilling: "Ansatt",
             arbeidssted: "RESTAURANTNAVN",
             stillingsprosent: 100,
             timelønn: 200,
-            bilde: "bildefil.png"
+            bilde: "bildefil.png",
+            telefon: "92356087"
         },
         {
-            id: 00002,
+            id: "00002",
             navn: "Anders Andersen",
             alder: 43,
             stilling: "Leder",
-            arbeidssted: "Gyldne Pizza Sentrum",
+            arbeidssted: "Sentrum",
             stillingsprosent: 100,
             timelønn: 250,
-            bilde: "bildefil.png"
+            bilde: "bildefil.png",
+            telefon: "92356087"
         },
     ];
 
     const getAllAnsatte = () => ansatte;
 
-    return {getAllAnsatte};
+    const getByArbeidssted = ( arbeidssted ) => ansatte.filter( ansatt => 
+        ansatt.arbeidssted.toLowerCase() === arbeidssted.toLowerCase() );
+        
+        
+    
+
+    return {getAllAnsatte,
+        getByArbeidssted
+    };
 
 }());
 
