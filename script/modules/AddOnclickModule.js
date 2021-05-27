a99a3a54bb36a3abbc05f8bbd4115d6d438eea04
Generoman <roman181@gmail.com>
2021-05-27 17:13:41 +0200

    Ferdig med "legg til" for restauranter/ansatte

--- script/modules/AddOnclickModule.js
+++ script/modules/AddOnclickModule.js
@@ -14,6 +14,14 @@ const AddOnclickModule = (function() {
 
         let allCards = document.querySelectorAll(`.${cardClass}`);
     
+        let confirmDeleteButton = document.querySelector("#confirm-delete-button");
+
+        let deleteInput = document.querySelector("#delete-input");
+
+        let deleteName = document.querySelector("#delete-name");
+
+        let deletePopup = document.querySelector("#delete-popup");
+
         allCards.forEach(card => {
     
             let connectedEditButton = document.querySelector(`#editBtn${card.id}`);
@@ -28,13 +36,27 @@ const AddOnclickModule = (function() {
     
             let connectedModalBackground = document.querySelector(`#modalBg${card.id}`);
 
-            let confirmDeleteButton = document.querySelector("#confirm-delete-button");
-
-            let deleteInput = document.querySelector("#delete-input");
-
-            let deleteName = document.querySelector("#delete-name");
-
-            let deletePopup = document.querySelector("#delete-popup");
+            connectedDeleteButton.addEventListener("click", () => {
+                
+                // Funksjon som sletter kort fra array
+                // Krever at siden den brukes på har et <input>-element med id="delete-input"
+                
+                // Hjelpefunksjon som brukes til å velge ID til objekt som skal slettes
+                 
+    
+                confirmDeleteButton.addEventListener("click", () => {
+                    if (deleteInput.value === "SLETT"){
+                        connectedArray.splice(connectedArray.findIndex(checkId), 1);
+                        deletePopup.classList.remove("is-active");
+                        deleteInput.value = "";
+                    };
+                });
+    
+                deleteName.innerHTML = `${connectedArray.filter(o => o.id == card.id)[0].navn}`;
+    
+                deletePopup.classList.add("is-active");
+    
+            });
     
             connectedEditButton.addEventListener("click", () => {
                 connectedPopup.classList.add("is-active");
@@ -48,34 +70,10 @@ const AddOnclickModule = (function() {
                 connectedPopup.classList.remove("is-active");
             });
 
-            connectedDeleteButton.addEventListener("click", () => {
-                
-                // Funksjon som sletter kort fra array
-                // Krever at siden den brukes på har et <input>-element med id="delete-input"
-                // Se eksempel i Restauranter.html, linje 36-57
-                
-                // Hjelpefunksjon som brukes til å velge ID til objekt som skal slettes
-                 
-
-                confirmDeleteButton.addEventListener("click", () => {
-                    if (deleteInput.value === "SLETT"){
-                        connectedArray.splice(connectedArray.findIndex(checkId), 1);
-                        deletePopup.classList.remove("is-active");
-                        deleteInput.value = "";
-                    };
-                });
-
-                deleteName.innerHTML = `${connectedArray.filter(o => o.id == card.id)[0].navn}`;
-
-                deletePopup.classList.add("is-active");
-
-            });
-
             connectedSaveButton.addEventListener("click", () => {
                 // Funksjon som lagrer endring i array
                 // Uferdig
-                let arrayObject = connectedArray[connectedArray.findIndex(checkId)];
-                if (arrayObject.hasOwnProperty("telefon")) {
+                if (connectedArray[0].hasOwnProperty("telefon")) {
                     editObject(connectedArray, lookUpArray);
                 } else {
                     // Uferdig
@@ -84,13 +82,11 @@ const AddOnclickModule = (function() {
             });
 
 
-
-            // Hjelpefunksjoner
+            // Hjelpefunksjoner i loop
 
             const editObject = (arrayToChange, lookUpArray) => {
                 let editPopup = document.querySelector(`#popup${card.id}`);
                 let inputFields = editPopup.querySelectorAll(".edit-input");
-                arrayToChange[arrayToChange.findIndex(checkId)]
                 inputFields.forEach(field => {
                     if (field.classList.contains("navn")) {
                         arrayToChange[arrayToChange.findIndex(checkId)].navn = field.value;
@@ -115,25 +111,77 @@ const AddOnclickModule = (function() {
                     }
                 });
             }
-
-            const getLeaderOrWorkPlace = (lookUpArray, id) => {
-                let objectName = "";
-                lookUpArray.forEach(o => {
-                    if (id == o.id) {
-                        objectName = o.navn;
-                    }
-                })
-                let leaderOrWorkplace = [objectName, id];
-                return leaderOrWorkplace;
-            }
             
             const checkId = (o) => o.id == card.id;
 
-
-
-
-
         });
+
+        
+
+        let confirmAddButton = document.querySelector("#confirm-add-button");
+
+        let addPopup = document.querySelector("#add-popup");
+
+        confirmAddButton.addEventListener("click", () => {
+            if (connectedArray[0].hasOwnProperty("telefon")) {
+                addNewObject(connectedArray, lookUpArray);
+            } else {
+                // Uferdig
+            }
+            addPopup.classList.remove("is-active");
+        });
+
+        
+
+        // Hjelpefunksjoner utenfor loop
+
+        const addNewObject = (arrayToChange, lookUpArray) => {
+            let addPopup = document.querySelector("#add-popup");
+            let inputFields = addPopup.querySelectorAll(".add-input");
+            let arrayLength = arrayToChange.length;
+            let newId = parseInt(arrayToChange[arrayLength-1].id)+1;
+            let newObject = {id: newId.toString()};
+            if (arrayToChange[0].hasOwnProperty("alder")) {
+                newObject.bilde = "../kokk.png";
+            } else {
+                newObject.bilde = "building.png";
+            }
+            inputFields.forEach(field => {
+                if (field.classList.contains("navn")) {
+                    newObject.navn = field.value;
+                } else if (field.classList.contains("adresse")) {
+                    newObject.adresse = field.value;
+                } else if (field.classList.contains("telefon")) {
+                    newObject.telefon = field.value;
+                } else if (field.classList.contains("kapasitet")) {
+                    newObject.kapasitet = parseInt(field.value);
+                } else if (field.classList.contains("leder")) {
+                    newObject.leder = getLeaderOrWorkPlace(lookUpArray, field.value);
+                } else if (field.classList.contains("alder")) {
+                    newObject.alder = parseInt(field.value);
+                } else if (field.classList.contains("stilling")) {
+                    newObject.stilling = field.value;
+                } else if (field.classList.contains("stillingsprosent")) {
+                    newObject.stillingsprosent = parseInt(field.value);
+                } else if (field.classList.contains("timelønn")) {
+                    newObject.timelønn = parseInt(field.value);
+                } else if (field.classList.contains("arbeidssted")) {
+                    newObject.arbeidssted = getLeaderOrWorkPlace(lookUpArray, field.value);
+                }
+            });
+            arrayToChange.push(newObject);
+        }
+
+        const getLeaderOrWorkPlace = (lookUpArray, id) => {
+            let objectName = "";
+            lookUpArray.forEach(o => {
+                if (id == o.id) {
+                    objectName = o.navn;
+                }
+            })
+            let leaderOrWorkplace = [objectName, id];
+            return leaderOrWorkplace;
+        }
     };
 
     return {addOnclickToButtons}
