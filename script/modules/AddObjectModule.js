cfd1cd17a6e2c5ba494aee24d650eaf9c08a0233
Generoman <roman181@gmail.com>
2021-05-28 20:27:47 +0200

    glemte en

--- script/modules/AddObjectModule.js
+++ script/modules/AddObjectModule.js
@@ -23,7 +23,7 @@ const AddObjectModule = (function(){
             } else if (field.classList.contains("kapasitet")) {
                 newObject.kapasitet = parseInt(field.value);
             } else if (field.classList.contains("leder")) {
-                newObject.leder = getLeaderOrWorkPlace(lookUpArray, field.value);
+                newObject.leder = HelperFunctionsModule.getLeaderOrWorkPlace(lookUpArray, field.value);
             } else if (field.classList.contains("alder")) {
                 newObject.alder = parseInt(field.value);
             } else if (field.classList.contains("stilling")) {
@@ -33,13 +33,46 @@ const AddObjectModule = (function(){
             } else if (field.classList.contains("timelønn")) {
                 newObject.timelønn = parseInt(field.value);
             } else if (field.classList.contains("arbeidssted")) {
-                newObject.arbeidssted = getLeaderOrWorkPlace(lookUpArray, field.value);
+                newObject.arbeidssted = HelperFunctionsModule.getLeaderOrWorkPlace(lookUpArray, field.value);
             }
         });
         arrayToChange.push(newObject);
     }
 
-    return {addNewObject}
+    const addNewFoodOrDrink = (connectedArray, lookUpArray) => {
+        let addPopup = document.querySelector("#add-popup");
+        let inputFields = addPopup.querySelectorAll(".add-input");
+        let ingredientFields = addPopup.querySelectorAll(".ingrediens");
+        let arrayLength = connectedArray.length;
+        let newId = parseInt(connectedArray[arrayLength-1].id)+1;
+        let newObject = {id: newId.toString()};
+        inputFields.forEach(field => {
+            if (field.classList.contains("navn")) {
+                newObject.navn = field.value;
+            } else if (field.classList.contains("kategori")) {
+                newObject.kategori = field.value;
+            } else if (field.classList.contains("type")) {
+                newObject.type = field.value;
+            } else if (field.classList.contains("kostnad")) {
+                newObject.kostnad = field.value;
+            } else if (field.classList.contains("pris")) {
+                newObject.pris = field.value;
+            }
+        });
+        if (newObject.kategori == "drikke") {
+            newObject.bilde = "beverage-icon.png";
+        } else {
+            newObject.bilde = "pizza-icon.png";
+            newObject.ingredienser = HelperFunctionsModule.getIngredientsFromInput(ingredientFields);
+            newObject.allergener = HelperFunctionsModule.getAllergensByIngredients(newObject.ingredienser, lookUpArray);
+            if (newObject.type == "rød pizza" || newObject.type == "hvit pizza") {
+                newObject.allergener.push("hvete");
+            }
+        }
+        connectedArray.push(newObject);
+    }
+
+    return {addNewObject, addNewFoodOrDrink}
 
 }());
 
