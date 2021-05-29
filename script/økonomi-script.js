9e4d33b8e10789e6d26e630cd160a9cac03470d8
Ellaoui94 <74535066+Ellaoui94@users.noreply.github.com>
2021-05-29 16:41:54 +0200

    økonomi nesten klar

--- script/økonomi-script.js
+++ script/økonomi-script.js
@@ -7,3 +7,18 @@ import IngredienserModule from "./modules/IngredienserModule.js";
 function calculateEconomy(restaurant){
 
 };
+
+// For Burger
+const navbarBurger = document.querySelector(".navbar-burger")
+const navbarMenu = document.querySelector("#nav-links");
+
+
+
+navbarBurger.addEventListener("click", ( e ) => {
+	navbarBurger.classList.toggle("is-active")
+	navbarMenu.classList.toggle("is-active");
+} );
+
+
+            
+          
