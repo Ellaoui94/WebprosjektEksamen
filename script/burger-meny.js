2b5b25c06ea7da9b95a74d0a1b1cd2290eda38eb
Saniya Baghirova <saniya.ibraimova@gmail.com>
2021-05-29 17:25:19 +0200

    added burger-meny.js

---
+++ script/burger-meny.js
@@ -0,0 +1,9 @@
+const navbarBurger = document.querySelector(".navbar-burger")
+const navbarMenu = document.querySelector("#nav-links");
+
+
+
+navbarBurger.addEventListener("click", ( e ) => {
+	navbarBurger.classList.toggle("is-active")
+	navbarMenu.classList.toggle("is-active");
+} );
