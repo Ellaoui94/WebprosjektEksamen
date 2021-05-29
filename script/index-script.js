01f2ea0538d35aa8e639c3bdb9f78c1afeee4040
Marina1014 <makh004@student.kristiania.no>
2021-05-29 12:39:46 +0200

    altered

--- script/index-script.js
+++ script/index-script.js
@@ -1,7 +1,18 @@
 const navbarBurger = document.querySelector(".navbar-burger")
 const navbarMenu = document.querySelector("#nav-links");
 
+
+
 navbarBurger.addEventListener("click", ( e ) => {
 	navbarBurger.classList.toggle("is-active")
 	navbarMenu.classList.toggle("is-active");
 } );
+
+
+            
+          
+          
+
+                     
+
+           
